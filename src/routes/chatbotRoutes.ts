20fa4b05e071d8ac4bffbc858e291041847f5ff6
Hanish Tharwani <hanish.tharwani2021@vitbhopal.ac.in>
2025-03-29 00:11:49 +0530

    first commit

--- src/routes/chatbotRoutes.ts
+++ src/routes/chatbotRoutes.ts
@@ -11,12 +11,18 @@ import {
 } from "../controllers/chatbotController";
 
 const router = express.Router();
-
+// @ts-ignore
 router.post("/generate", generateChatbot);
+// @ts-ignore
 router.post("/preview-response", previewResponse);
+// @ts-ignore
 router.post("/automation", saveChatbotAutomation);
+// @ts-ignore
 router.get("/automation", getChatbotAutomation);
+// @ts-ignore
 router.get("/account", getInstagramAccount);
+// @ts-ignore
 router.get("/posts", getChatbotInstagramPosts);
+// @ts-ignore
 router.all("/webhook", processChatbotWebhook);
 export default router;
--- src/routes/instagramRoutes.ts
+++ src/routes/instagramRoutes.ts
@@ -14,19 +14,27 @@ import { config } from "../config";
 const router = express.Router();
 
 // Authentication routes
+// @ts-ignore
 router.get("/connect", connectInstagram);
+// @ts-ignore
 router.get("/callback", instagramCallback);
 
 // Account and Post routes (pass userId as query parameter)
+// @ts-ignore
 router.get("/account", getInstagramAccount);
+// @ts-ignore
 router.get("/posts", getInstagramPosts);
 
 // Automation routes (pass userId as query parameter)
+// @ts-ignore
 router.post("/automation", saveCommentAutomation);
+// @ts-ignore
 router.get("/automation", getCommentAutomation);
 
 // Webhook endpoint
+// @ts-ignore
 router.get("/webhook", processWebhook);
+// @ts-ignore
 router.post("/webhook", processWebhook);
 
 export default router;
